b842a13e9e9b0fb68668355c6b8d875918682b71
Stephone5 <s.barton.ok@gmail.com>
2026-07-28 21:51:20 -0400

    Icon: yellow crown on charcoal as the Tribal Vend app icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Tribal Vend service worker — offline shell + notification display.
-const CACHE = "tv-v10";
+const CACHE = "tv-v11";
 const ASSETS = ["./","./index.html","./app.js","./data.js","./closet.js","./api.js","./manifest.webmanifest",
   "./icons/icon-192.png","./icons/icon-512.png"];
 
