4533818d689da0d86979d0a8b9fad7c8ca917b60
Stephone5 <s.barton.ok@gmail.com>
2026-08-01 00:20:31 -0400

    Inventory: full 41-item closet seed from live Sortly (810 units, real folders + SIDs)

--- closet-seed.js
+++ closet-seed.js
@@ -1,160 +1,375 @@
-// Initial closet contents, imported from the Sortly export (Jul 2026).
-// Used once, the first time the closet is opened on a fresh install.
+// Full inventory from Stephen's live Sortly (transcribed Aug 2026). Stable IDs = Sortly SIDs.
 export const CLOSET_SEED = {
-  "items": [
-    {
-      "id": "isgz6n7m",
-      "folder": "Candy",
-      "name": "Jack links",
-      "price": 1.2,
-      "qty": 2,
-      "min": 3,
-      "img": null
-    },
-    {
-      "id": "idgq066l",
-      "folder": "Cold Food",
-      "name": "Hot Pockets",
-      "price": 0.79,
-      "qty": 14,
-      "min": 15,
-      "img": null
-    },
-    {
-      "id": "iq2zzqxn",
-      "folder": "Cold Food",
-      "name": "Jimmy Dean SEC",
-      "price": 1,
-      "qty": 7,
-      "min": 12,
-      "img": null
-    },
-    {
-      "id": "icl1dpb3",
-      "folder": "Cold Food",
-      "name": "Uncrustables",
-      "price": 0.78,
-      "qty": 13,
-      "min": 16,
-      "img": null
-    },
-    {
-      "id": "ilm90mwe",
-      "folder": "Drinks",
-      "name": "12 ounce C4",
-      "price": 2.33,
-      "qty": 4,
-      "min": 20,
-      "img": null
-    },
-    {
-      "id": "iimbjfxj",
-      "folder": "Drinks",
-      "name": "Black Rifle Coffee",
-      "price": 1.67,
-      "qty": 10,
-      "min": 11,
-      "img": null
-    },
-    {
-      "id": "ikqcdis2",
-      "folder": "Drinks",
-      "name": "Celsius",
-      "price": 1,
-      "qty": 16,
-      "min": 20,
-      "img": null
-    },
-    {
-      "id": "i53qtg27",
-      "folder": "Drinks",
-      "name": "Coffee Monster",
-      "price": 2.02,
-      "qty": 7,
-      "min": 11,
-      "img": null
-    },
-    {
-      "id": "iwnkmyru",
-      "folder": "Drinks",
-      "name": "Coke",
-      "price": 0.71,
-      "qty": 4,
-      "min": 11,
-      "img": null
-    },
-    {
-      "id": "ie2c3v20",
-      "folder": "Drinks",
-      "name": "Dr Pepper",
-      "price": 0.67,
-      "qty": 18,
-      "min": 23,
-      "img": null
-    },
-    {
-      "id": "irodc31x",
-      "folder": "Drinks",
-      "name": "Gatorade Blue",
-      "price": 0.77,
-      "qty": 25,
-      "min": 27,
-      "img": null
-    },
-    {
-      "id": "ip4jqpnt",
-      "folder": "Drinks",
-      "name": "Mtn Dew",
-      "price": 0.62,
-      "qty": 14,
-      "min": 36,
-      "img": null
-    },
-    {
-      "id": "igmzv89z",
-      "folder": "Drinks",
-      "name": "Snapple",
-      "price": 0.88,
-      "qty": 6,
-      "min": 10,
-      "img": null
-    },
-    {
-      "id": "il9r4yaz",
-      "folder": "Drinks",
-      "name": "Sparkling ICE",
-      "price": 0.67,
-      "qty": 7,
-      "min": 9,
-      "img": null
-    },
-    {
-      "id": "immnordt",
-      "folder": "Snacks",
-      "name": "Muffins",
-      "price": 0.95,
-      "qty": 7,
-      "min": 10,
-      "img": null
-    },
-    {
-      "id": "ibv8m7q7",
-      "folder": "Snacks",
-      "name": "Snyders Pretzel peices",
-      "price": 0.68,
-      "qty": 15,
-      "min": 30,
-      "img": null
-    },
-    {
-      "id": "izznfbqd",
-      "folder": "Snacks",
-      "name": "Sun Chips",
-      "price": 0.62,
-      "qty": 9,
-      "min": 20,
-      "img": null
-    }
-  ],
-  "hist": []
+ "items": [
+  {
+   "id": "SJA07T0001",
+   "folder": "Drinks",
+   "name": "Mtn Dew",
+   "price": 0.62,
+   "qty": 14,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0002",
+   "folder": "Drinks",
+   "name": "Gatorade Blue",
+   "price": 0.77,
+   "qty": 25,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0003",
+   "folder": "Drinks",
+   "name": "Arnold Palmer",
+   "price": 0.45,
+   "qty": 26,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0006",
+   "folder": "Drinks",
+   "name": "Celsius",
+   "price": 1,
+   "qty": 16,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0007",
+   "folder": "Drinks",
+   "name": "Vitamin Water",
+   "price": 1,
+   "qty": 20,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0008",
+   "folder": "Drinks",
+   "name": "Sparkling ICE",
+   "price": 0.67,
+   "qty": 7,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0010",
+   "folder": "Drinks",
+   "name": "Sprite",
+   "price": 0.71,
+   "qty": 39,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0011",
+   "folder": "Drinks",
+   "name": "Pepsi",
+   "price": 0.62,
+   "qty": 15,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0013",
+   "folder": "Drinks",
+   "name": "Dr Pepper",
+   "price": 0.67,
+   "qty": 18,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0014",
+   "folder": "Drinks",
+   "name": "Coke",
+   "price": 0.71,
+   "qty": 4,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0015",
+   "folder": "Drinks",
+   "name": "Red Bull",
+   "price": 1.46,
+   "qty": 30,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0016",
+   "folder": "Cold Food",
+   "name": "Jimmy Dean SEC",
+   "price": 1,
+   "qty": 7,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0017",
+   "folder": "Cold Food",
+   "name": "Uncrustables",
+   "price": 0.78,
+   "qty": 13,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0019",
+   "folder": "Drinks",
+   "name": "Monster",
+   "price": 1.53,
+   "qty": 42,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0025",
+   "folder": "Snacks",
+   "name": "Miss Vickies",
+   "price": 0.62,
+   "qty": 47,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0026",
+   "folder": "Candy",
+   "name": "KitKat",
+   "price": 0.94,
+   "qty": 19,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0029",
+   "folder": "Snacks",
+   "name": "Snyders Pretzel peices",
+   "price": 0.68,
+   "qty": 15,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0030",
+   "folder": "Snacks",
+   "name": "CheezIt",
+   "price": 0.55,
+   "qty": 47,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0031",
+   "folder": "Candy",
+   "name": "Nutty Buddy",
+   "price": 0.57,
+   "qty": 10,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0032",
+   "folder": "Candy",
+   "name": "Reese's",
+   "price": 0.77,
+   "qty": 37,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0034",
+   "folder": "Candy",
+   "name": "Trail Mix",
+   "price": 0.41,
+   "qty": 22,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0035",
+   "folder": "Candy",
+   "name": "Clif bars",
+   "price": 1.05,
+   "qty": 17,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0036",
+   "folder": "Snacks",
+   "name": "PopTarts",
+   "price": 0.21,
+   "qty": 52,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0038",
+   "folder": "Candy",
+   "name": "Peanut M&M's",
+   "price": 0.94,
+   "qty": 28,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0040",
+   "folder": "Drinks",
+   "name": "12 ounce C4",
+   "price": 2.33,
+   "qty": 4,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0043",
+   "folder": "Drinks",
+   "name": "Coffee Monster",
+   "price": 2.02,
+   "qty": 7,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0046",
+   "folder": "Snacks",
+   "name": "Sun Chips",
+   "price": 0.62,
+   "qty": 9,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0048",
+   "folder": "Candy",
+   "name": "Trident",
+   "price": 0.45,
+   "qty": 14,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0050",
+   "folder": "Candy",
+   "name": "Payday",
+   "price": 0.95,
+   "qty": 15,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0053",
+   "folder": "Candy",
+   "name": "Butterfinger",
+   "price": 0.77,
+   "qty": 31,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0054",
+   "folder": "Snacks",
+   "name": "Honey Buns",
+   "price": 0.6,
+   "qty": 11,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0055",
+   "folder": "Snacks",
+   "name": "Muffins",
+   "price": 0.95,
+   "qty": 7,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0056",
+   "folder": "Drinks",
+   "name": "Black Rifle Coffee",
+   "price": 1.67,
+   "qty": 10,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0057",
+   "folder": "Snacks",
+   "name": "Ruffles",
+   "price": 0.62,
+   "qty": 21,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0058",
+   "folder": "Drinks",
+   "name": "Water",
+   "price": 0.2,
+   "qty": 24,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0063",
+   "folder": "Candy",
+   "name": "Oatmeal Cream Pie",
+   "price": 0.45,
+   "qty": 11,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0064",
+   "folder": "Drinks",
+   "name": "AriZona Green Tea",
+   "price": 0.45,
+   "qty": 20,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0071",
+   "folder": "Cold Food",
+   "name": "Hot Pockets",
+   "price": 0.79,
+   "qty": 14,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0072",
+   "folder": "Snacks",
+   "name": "Taki's",
+   "price": 0.7,
+   "qty": 34,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0073",
+   "folder": "Candy",
+   "name": "Jack links",
+   "price": 1.2,
+   "qty": 2,
+   "min": "",
+   "img": null
+  },
+  {
+   "id": "SJA07T0074",
+   "folder": "Drinks",
+   "name": "Snapple",
+   "price": 0.88,
+   "qty": 6,
+   "min": "",
+   "img": null
+  }
+ ],
+ "hist": []
 };
